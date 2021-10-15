ec45882ce822d2dc215a33622e1dc29326746145
Dominik Haeusser <haeusser-dominik@web.de>
2021-10-15 06:26:44 +0200

    add a grid system

--- src/App.js
+++ src/App.js
@@ -5,12 +5,45 @@ function App() {
     <div className="App">
       <h1>music-list-app</h1>
       <div className="music-list">
-        <h2>put here music list</h2>
-        <p>no</p>
-        <p>title</p>
-        <p>interpret</p>
-        <p>leadsheet</p>
-        <p>anhören</p>
+        <div className="name-row">
+          <div className="col-name no">
+            <h2>NO</h2>
+          </div>
+          <div className="col-name title">
+            <h2>Titel</h2>
+          </div>
+          <div className="col-name interpreter">
+            <h2>Interpreter</h2>
+          </div>
+        </div>
+        {/* <div className="leadsheet">
+          <p>leadsheet</p>
+        </div>
+        <div className="listen">
+          <p>anhören</p>
+        </div> */}
+        <div className="song-row odd">
+          <div className="song-no border">
+            <p>1</p>
+          </div>
+          <div className="song-title border">
+            <p>Toxic</p>
+          </div>
+          <div className="song-interpreter border">
+            <p>Britney Spears</p>
+          </div>
+        </div>
+        <div className="song-row even">
+          <div className="song-no border">
+            <p>2</p>
+          </div>
+          <div className="song-title border">
+            <p>Strobe</p>
+          </div>
+          <div className="song-interpreter border">
+            <p>Deadmau5</p>
+          </div>
+        </div>
       </div>
     </div>
   )
